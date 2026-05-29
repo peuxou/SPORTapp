8ab12c03df4bb7f682997787a41816bdf3296847
peuxou <132053903+peuxou@users.noreply.github.com>
2026-05-29 14:33:48 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'sport-app-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/2936/2936886.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+});
